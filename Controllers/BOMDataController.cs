adb9a3ca6bdac498aa969cc6cdba47735a8f1d41
Dan Colgan <dancolgan11@comcast.net>
2020-06-26 21:09:32 -0400

    Add project files.

---
+++ Controllers/BOMDataController.cs
@@ -0,0 +1,49 @@
+﻿using BOM_V2.Controllers.helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BOM_V2.Controllers;
+using BOM_V2.Models;
+
+namespace BOM_V2.Controllers
+{
+    public class BOMDataController : ApiController
+    {
+        #region IS THIS USER AN ADMIN
+        [HttpGet]
+        [Route("api/Database/InsertIncident")]
+        public bool InsertIncident(BoroughIncident INC)
+        {
+            try
+            {
+                using (BoroEntities BD = new BoroEntities())
+                {
+                    BoroughIncident BI = new BoroughIncident();
+                    BI.IncidentStatusID = INC.IncidentStatusID;
+                    BI.IncidentTypeID = INC.IncidentTypeID;
+                    BI.IncidentReportDate = INC.IncidentReportDate;
+                    BI.IncidentReportedBy = INC.IncidentReportedBy;
+                    BI.IncidentReportedByContactPhone = INC.IncidentReportedByContactPhone;
+                    BI.LastUpdatedBy = INC.LastUpdatedBy;
+                    BI.ViolationCode = INC.ViolationCode;
+                    BI.IncidentReportText = INC.IncidentReportText;
+                    BI.IncidentStreetAddress1 = INC.IncidentStreetAddress1;
+                    BI.IncidentStreetAddress2 = INC.IncidentStreetAddress2;
+                    BD.BoroughIncidents.Add(BI);
+                    BD.SaveChanges();
+
+                    return true;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Controllers/helpers/Incident.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BOM_V2.Controllers.helpers
+{
+    public class Incident
+    {
+        int IncidentStatusID { get; set; }
+        int IncidentTypeID { get; set; }
+        DateTime IncidentReportDate { get; set; }
+        DateTime IncidentLastUpdated { get; set; }
+        string LastUpdatedBy { get; set; }
+        string IncidentReportedBy { get; set; }
+        string IncidentReportedByContactPhone { get; set; }
+        string ViolationCode { get; set; }
+        string IncidentReportText { get; set; }
+        string IncidentStreetAddress1 { get; set; }
+        string IncidentStreetAddress2 { get; set; }
+    }
+}
